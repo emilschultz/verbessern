1fea0f98ca548247ff84716108d82e3ae11096e6
emilschultz <emil.schultz@amedia.no>
2023-03-25 22:35:28 +0100

    initial commit: init gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@ module.exports = {
     title: `verbessern`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "9fmrkte8",
+        dataset: "production",
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.MY_SANITY_TOKEN,
+      },
+    },
+  ],
   plugins: [],
-}
+};
