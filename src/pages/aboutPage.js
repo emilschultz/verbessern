dbbc850b11d18e96819a198e3f73094ccce0f80b
emilschultz <emil.schultz@amedia.no>
2023-03-26 14:36:22 +0200

    use Layout component for navigation/routing

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,17 +1,17 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/Layout";
 
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About</h1>
-      <p>
-        My name is Emil. I write to get better at programming. This blog is
-        mostly for my self, but if anyone else learns something along the way,
-        then thats just awesome!
-      </p>
-      <Link to="/">Back home</Link>
-    </main>
+    <Layout pageTitle="About">
+      <main>
+        <p>
+          My name is Emil. I write to get better at programming. This blog is
+          mostly for my self, but if anyone else learns something along the way,
+          then thats just awesome!
+        </p>
+      </main>
+    </Layout>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Verbessern</h1>
-      <h2>
-        Verbessern means "To imprve". I will try to improve my knowledge on
-        writing about frontend technologies, patterns, best practices, and much
-        more. You can improve yours by reading about it
-      </h2>
-      <Link to="aboutPage">About</Link>
-    </main>
+    <Layout pageTitle="Verbessern">
+      <main>
+        <h2>
+          Verbessern means "To improve". I will try to improve my knowledge on
+          writing about frontend technologies, patterns, best practices, and
+          much more. You can improve yours by reading about it
+        </h2>
+      </main>
+    </Layout>
   );
 };
 
