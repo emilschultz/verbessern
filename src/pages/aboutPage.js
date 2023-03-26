1e99a278f648e58b089eb835c77902d2ac1c07e4
emilschultz <emil.schultz@amedia.no>
2023-03-26 15:08:04 +0200

    update layout component

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,16 +1,14 @@
 import * as React from "react";
-import Layout from "../components/Layout";
+import Layout from "../components/layout/Layout";
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About">
-      <main>
-        <p>
-          My name is Emil. I write to get better at programming. This blog is
-          mostly for my self, but if anyone else learns something along the way,
-          then thats just awesome!
-        </p>
-      </main>
+      <p>
+        My name is Emil. I write to get better at programming. This blog is
+        mostly for my self, but if anyone else learns something along the way,
+        then thats just awesome!
+      </p>
     </Layout>
   );
 };
